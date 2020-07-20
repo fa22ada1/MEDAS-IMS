5d97c4fd03c98b3fbaf8ff10012c6bb53511c25b
OussamaAI <47124705+OussamaAI@users.noreply.github.com>
2020-07-20 10:07:06 +0100

    utils.Conn + entites.Utilisateurs

---
+++ IMS/src/entities/Utilisateur.java
@@ -0,0 +1,48 @@
+package entities;
+
+import java.sql.ResultSet;
+
+import utils.Conn;
+
+public class Utilisateur {
+	int ID;
+	String name;
+	String title;
+	int rang;
+	
+	public Utilisateur(int ID) {
+		this.ID = ID;
+		try {
+			Conn conn = new Conn();
+			ResultSet rs;
+			String q = "Select * from Utilisateurs where ID =" + ID;
+			rs = conn.s.executeQuery(q);
+			rs.next();
+			
+			name = rs.getString("nom");
+			title = rs.getString("titre");
+			rang = rs.getInt("rang");
+		} catch(Exception e1){
+			e1.printStackTrace();
+		}
+	}
+	
+	public String Tag() {
+		return name + "-" + title;
+	}
+	
+	public static int getID(String name, String PW) {
+		try {
+			Conn conn = new Conn();
+			ResultSet rs;
+			String q = "Select ID from Utilisateurs where nom ='" + name + "' and MP = UNHEX(SHA1('"+PW+"')";
+			rs = conn.s.executeQuery(q);
+			if(rs.next()) {
+				return rs.getInt("ID");
+			}
+		} catch(Exception e1){
+			e1.printStackTrace();
+		}
+		return -1;
+	}
+}
---
+++ IMS/src/utils/Conn.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+	public Connection c;
+	public Statement s;
+	public static String dbName = "ims";
+	public static String dbUser = "root";
+	public static String dbPW = "oussama";
+	
+	public Conn() {
+    	try {	
+    		this.c = DriverManager.getConnection("jdbc:mysql://localhost/" + dbName, dbUser, dbPW);
+    		s = c.createStatement();
+    	} catch(Exception E) {
+    		E.printStackTrace();
+    	}
+    }
+}
